17589f64c9d90771e8f43b2e241df3be45625eb5
Eduinfer03 <eduin_quintero@soy.sena.edu.co>
2023-09-13 20:42:59 -0500

    API_V3
    refactor(authentication): Endpoints de login y password-update simplificados
    
    - Se simplificaron los endpoints de 'login' y 'password-update' para unificarlos en un solo endpoint en cada caso.
    - Esto mejora la consistencia en el manejo de autenticación y actualización de contraseñas.

--- Proyecto_API/Backend/routes/passwordUpdate.js
+++ Proyecto_API/Backend/routes/passwordUpdate.js
@@ -3,35 +3,44 @@ const router = express.Router();
 const connection = require('../db');
 const bcrypt = require('bcrypt');
 
-// Cambiar la contraseña de un administrador permitiendo la contraseña actual en texto sin cifrar
-router.put('/administrador/:tdoc_admin/:id_admin', (req, res) => {
-  const { tdoc_admin, id_admin } = req.params;
+router.put('/:tdoc/:id', (req, res) => {
+  const { tdoc, id } = req.params;
   const { current_pass, new_pass } = req.body;
 
   if (!current_pass || !new_pass) {
     return res.status(400).json({ error: 'Los campos de contraseña actual y nueva contraseña son obligatorios' });
   }
 
-  // Obtener la contraseña actual almacenada en la base de datos
-  connection.query(
-    'SELECT pass_admin FROM administrador WHERE tdoc_admin = ? AND id_admin = ?',
-    [tdoc_admin, id_admin],
-    (err, results) => {
+  const isAdminQuery = 'SELECT * FROM administrador WHERE tdoc_admin = ? AND id_admin = ?';
+  const isEmployeeQuery = 'SELECT * FROM empleado WHERE tdoc_empleado = ? AND id_empleado = ?';
+
+  connection.query(isAdminQuery, [tdoc, id], (err, adminResults) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Error al obtener el administrador' });
+      return;
+    }
+
+    connection.query(isEmployeeQuery, [tdoc, id], (err, employeeResults) => {
       if (err) {
         console.error(err);
-        res.status(500).json({ error: 'Error al obtener la contraseña actual' });
+        res.status(500).json({ error: 'Error al obtener el empleado' });
         return;
       }
 
-      if (results.length === 0) {
-        res.status(404).json({ error: 'Administrador no encontrado' });
+      if (adminResults.length === 0 && employeeResults.length === 0) {
+        res.status(404).json({ error: 'Usuario no encontrado' });
         return;
       }
 
-      const storedPassword = results[0].pass_admin;
+      const isAdministrator = adminResults.length > 0;
+      const storedPassword = isAdministrator ? adminResults[0].pass_admin : employeeResults[0].pass_empleado;
 
-      // Verificar si la contraseña actual proporcionada está encriptada
-      if (bcrypt.compareSync(current_pass, storedPassword)) {
+      // Verificar si la contraseña actual proporcionada coincide con la almacenada (ya sea encriptada o no)
+      const isMatchEncrypted = bcrypt.compareSync(current_pass, storedPassword);
+      const isMatchUnencrypted = current_pass === storedPassword;
+
+      if ((isMatchEncrypted || isMatchUnencrypted)) {
         // Generar una sal (salt) para encriptar la nueva contraseña
         const saltRounds = 10;
 
@@ -44,146 +53,23 @@ router.put('/administrador/:tdoc_admin/:id_admin', (req, res) => {
           }
 
           // Actualizar la contraseña en la base de datos
-          connection.query(
-            'UPDATE administrador SET pass_admin = ? WHERE tdoc_admin = ? AND id_admin = ?',
-            [hashedPassword, tdoc_admin, id_admin],
-            (err, result) => {
-              if (err) {
-                console.error(err);
-                res.status(500).json({ error: 'Error al cambiar la contraseña del administrador' });
-                return;
-              }
-              res.json({ message: 'Contraseña del administrador cambiada con éxito' });
-            }
-          );
-        });
-      } else {
-        // La contraseña actual proporcionada no coincide con la almacenada encriptada
-        // Verificar si coincide con la contraseña almacenada sin cifrar
-        if (current_pass === storedPassword) {
-          // Generar una sal (salt) para encriptar la nueva contraseña
-          const saltRounds = 10;
+          const updateQuery = isAdministrator ? 'UPDATE administrador SET pass_admin = ?' : 'UPDATE empleado SET pass_empleado = ?';
+          const params = [hashedPassword, tdoc, id];
 
-          // Encriptar la nueva contraseña
-          bcrypt.hash(new_pass, saltRounds, (err, hashedPassword) => {
+          connection.query(updateQuery, params, (err, result) => {
             if (err) {
               console.error(err);
-              res.status(500).json({ error: 'Error al encriptar la nueva contraseña' });
+              res.status(500).json({ error: 'Error al cambiar la contraseña del usuario' });
               return;
             }
-
-            // Actualizar la contraseña en la base de datos
-            connection.query(
-              'UPDATE administrador SET pass_admin = ? WHERE tdoc_admin = ? AND id_admin = ?',
-              [hashedPassword, tdoc_admin, id_admin],
-              (err, result) => {
-                if (err) {
-                  console.error(err);
-                  res.status(500).json({ error: 'Error al cambiar la contraseña del administrador' });
-                  return;
-                }
-                res.json({ message: 'Contraseña del administrador cambiada con éxito' });
-              }
-            );
+            res.json({ message: 'Contraseña cambiada con éxito' });
           });
-        } else {
-          res.status(401).json({ error: 'Contraseña actual incorrecta' });
-        }
-      }
-    }
-  );
-});
-
-// Cambiar la contraseña de un empleado permitiendo la contraseña actual en texto sin cifrar
-router.put('/empleado/:tdoc_empleado/:id_empleado', (req, res) => {
-  const { tdoc_empleado, id_empleado } = req.params;
-  const { current_pass, new_pass } = req.body;
-
-  if (!current_pass || !new_pass) {
-    return res.status(400).json({ error: 'Los campos de contraseña actual y nueva contraseña son obligatorios' });
-  }
-
-  // Obtener la contraseña actual almacenada en la base de datos
-  connection.query(
-    'SELECT pass_empleado FROM empleado WHERE tdoc_empleado = ? AND id_empleado = ?',
-    [tdoc_empleado, id_empleado],
-    (err, results) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Error al obtener la contraseña actual' });
-        return;
-      }
-
-      if (results.length === 0) {
-        res.status(404).json({ error: 'Empleado no encontrado' });
-        return;
-      }
-
-      const storedPassword = results[0].pass_empleado;
-
-      // Verificar si la contraseña actual proporcionada está encriptada
-      if (bcrypt.compareSync(current_pass, storedPassword)) {
-        // Generar una sal (salt) para encriptar la nueva contraseña
-        const saltRounds = 10;
-
-        // Encriptar la nueva contraseña
-        bcrypt.hash(new_pass, saltRounds, (err, hashedPassword) => {
-          if (err) {
-            console.error(err);
-            res.status(500).json({ error: 'Error al encriptar la nueva contraseña' });
-            return;
-          }
-
-          // Actualizar la contraseña en la base de datos
-          connection.query(
-            'UPDATE empleado SET pass_empleado = ? WHERE tdoc_empleado = ? AND id_empleado = ?',
-            [hashedPassword, tdoc_empleado, id_empleado],
-            (err, result) => {
-              if (err) {
-                console.error(err);
-                res.status(500).json({ error: 'Error al cambiar la contraseña del empleado' });
-                return;
-              }
-              res.json({ message: 'Contraseña del empleado cambiada con éxito' });
-            }
-          );
         });
       } else {
-        // La contraseña actual proporcionada no coincide con la almacenada encriptada
-        // Verificar si coincide con la contraseña almacenada sin cifrar
-        if (current_pass === storedPassword) {
-          // Generar una sal (salt) para encriptar la nueva contraseña
-          const saltRounds = 10;
-
-          // Encriptar la nueva contraseña
-          bcrypt.hash(new_pass, saltRounds, (err, hashedPassword) => {
-            if (err) {
-              console.error(err);
-              res.status(500).json({ error: 'Error al encriptar la nueva contraseña' });
-              return;
-            }
-
-            // Actualizar la contraseña en la base de datos
-            connection.query(
-              'UPDATE empleado SET pass_empleado = ? WHERE tdoc_empleado = ? AND id_empleado = ?',
-              [hashedPassword, tdoc_empleado, id_empleado],
-              (err, result) => {
-                if (err) {
-                  console.error(err);
-                  res.status(500).json({ error: 'Error al cambiar la contraseña del empleado' });
-                  return;
-                }
-                res.json({ message: 'Contraseña del empleado cambiada con éxito' });
-              }
-            );
-          });
-        } else {
-          res.status(401).json({ error: 'Contraseña actual incorrecta' });
-        }
+        res.status(401).json({ error: 'Contraseña actual incorrecta' });
       }
-    }
-  );
+    });
+  });
 });
-
 
 module.exports = router;
